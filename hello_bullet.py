85fc83fa8329342dc24066632baa9c6543df54ae
ggldnl <danielgigliotti99.dg@gmail.com>
2024-11-14 18:31:54 +0100

    Added right leg urdf

--- hello_bullet.py
+++ hello_bullet.py
@@ -7,13 +7,13 @@ p.setGravity(0,0,-10)
 planeId = p.loadURDF("plane.urdf")
 cubeStartPos = [0,0,0]
 cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
-robotId = p.loadURDF("hexapod.urdf",cubeStartPos, cubeStartOrientation, 
+robotID = p.loadURDF("right_leg.urdf",cubeStartPos, cubeStartOrientation, 
                    # useMaximalCoordinates=1, ## New feature in Pybullet
                    flags=p.URDF_USE_INERTIA_FROM_FILE)
 for i in range (10000):
     p.stepSimulation()
     time.sleep(1./240.)
-cubePos, cubeOrn = p.getBasePositionAndOrientation(robotId)
+cubePos, cubeOrn = p.getBasePositionAndOrientation(robotID)
 print(cubePos,cubeOrn)
 p.disconnect()
 
